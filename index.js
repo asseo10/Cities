50384cea9d6e0ed15abf3f09897700d6b560f81a
Asir Nael <asirnael10@gmail.com>
2024-11-25 12:10:15 +0100

    Update city header

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const targetCityHeader = document.getElementById("target-city-header");
 const closestSpan = document.getElementById("closest");
 const furthestSpan = document.getElementById("furthest");
 const tableContainer = document.getElementById("table");
+
+const updateCityHeader = (cityName, error = false) => {
+    targetCityHeader.textContent = error
+        ? `${cityName} finns inte i databasen`
+        : cityName;
+};
