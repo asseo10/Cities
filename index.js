d203527f25795b8cab940ce089594cef320d64fc
Asir Nael <asirnael10@gmail.com>
2024-11-25 12:06:49 +0100

    HTML references

---
+++ index.js
@@ -0,0 +1,5 @@
+const citiesContainer = document.getElementById("cities");
+const targetCityHeader = document.getElementById("target-city-header");
+const closestSpan = document.getElementById("closest");
+const furthestSpan = document.getElementById("furthest");
+const tableContainer = document.getElementById("table");
